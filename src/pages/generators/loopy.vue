8f9bab787c3548cd10e042bbc25aae0d81b9832c
Arsen21312 <arsenidmitriy@gmail.com>
2026-01-06 11:34:01 +0500

    Add local Loopy bundle and generator page

---
+++ src/pages/generators/loopy.vue
@@ -0,0 +1,25 @@
+﻿<template>
+  <div class="loopy-shell">
+    <iframe
+      class="loopy-frame"
+      src="/loopy/"
+      title="Loopy"
+      loading="lazy"
+    ></iframe>
+  </div>
+</template>
+
+<style scoped>
+.loopy-shell {
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+}
+
+.loopy-frame {
+  display: block;
+  width: 100%;
+  height: 100%;
+  border: 0;
+}
+</style>
