0c0c0b79061690c1e280f3e8f1e251f455bfab86
Arsen21312 <arsenidmitriy@gmail.com>
2026-01-10 01:45:52 +0500

    нет рекламы у ведьмы

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="layout-shell">
+    <ClientOnly>
+      <RippleGrid
+        class="ripple-grid-layer"
+        :enable-rainbow="false"
+        :ripple-intensity="0.01"
+        :grid-size="20"
+        :grid-thickness="20"
+        :mouse-interaction="true"
+        :mouse-interaction-radius="1.2"
+        :opacity="0.85"
+        grid-color="#d6e6ff"
+      />
+    </ClientOnly>
     <GlobalSnow />
     <MainHeader />
 
@@ -21,6 +34,7 @@ import MainFooter from '@/components/navigation/MainFooter.vue'
 import YandexRtbBlock from '@/components/ads/YandexRtbBlock.vue'
 import YandexRtbFloorAd from '@/components/ads/YandexRtbFloorAd.vue'
 import GlobalSnow from '@/components/ui/GlobalSnow.vue'
+import RippleGrid from '@/components/ui/RippleGrid.vue'
 import CookieBanner from '@/components/CookieBanner.vue'
 import faviconUrl from '~/assets/images/wolf-favicon.png'
 
@@ -65,9 +79,15 @@ useHead(() => ({
 <style scoped>
 .layout-shell {
   overflow-x: hidden;
+  position: relative;
 }
 
 .main-content {
   overflow-x: hidden;
+}
+
+.layout-shell > :not(.ripple-grid-layer) {
+  position: relative;
+  z-index: 2;
 }
 </style>
